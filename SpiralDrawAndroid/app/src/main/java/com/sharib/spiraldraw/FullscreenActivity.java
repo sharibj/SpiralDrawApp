f010e71445d10c8fe9c719eaa766fe64ef71b8d5
sjafari <sjafari@hytrust.com>
2019-03-14 23:06:50 +0530

    Added Download feature. Works in HTML app. Doesn't work in android app

--- SpiralDrawAndroid/app/src/main/java/com/sharib/spiraldraw/FullscreenActivity.java
+++ SpiralDrawAndroid/app/src/main/java/com/sharib/spiraldraw/FullscreenActivity.java
@@ -1,10 +1,32 @@
 package com.sharib.spiraldraw;
 
+import android.app.DownloadManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * @author Sharib Jafari
@@ -20,6 +42,33 @@ public class FullscreenActivity extends AppCompatActivity {
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setLoadWithOverviewMode(true);
         mWebView.getSettings().setUseWideViewPort(true);
+        mWebView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent,
+                                        String contentDisposition, String mimeType,
+                                        long contentLength) {
+
+                if (url.startsWith("data:")) {  //when url is base64 encoded data
+                    String path = createAndSaveFileFromBase64Url(url);
+                    return;
+                }
+
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setMimeType(mimeType);
+                String cookies = CookieManager.getInstance().getCookie(url);
+                request.addRequestHeader("cookie", cookies);
+                request.addRequestHeader("User-Agent", userAgent);
+                request.setDescription(getResources().getString(R.string.msg_downloading));
+                String filename = URLUtil.guessFileName(url, contentDisposition, mimeType);
+                request.setTitle(filename);
+                request.allowScanningByMediaScanner();
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), R.string.msg_downloading, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
@@ -35,6 +84,71 @@ public class FullscreenActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        mWebView.loadUrl("file:///android_asset/allInOne.html");
+        mWebView.loadUrl("file:///android_asset/spiral.html");
+    }
+    public String createAndSaveFileFromBase64Url(String url) {
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+        String filetype = url.substring(url.indexOf("/") + 1, url.indexOf(";"));
+        String filename = System.currentTimeMillis() + "." + filetype;
+        File file = new File(path, filename);
+        try {
+            if(!path.exists())
+                path.mkdirs();
+            if(!file.exists()) {
+                try {
+                    file.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            String base64EncodedString = url.substring(url.indexOf(",") + 1);
+            byte[] decodedBytes = Base64.decode(base64EncodedString, Base64.DEFAULT);
+            OutputStream os = new FileOutputStream(file);
+            os.write(decodedBytes);
+            os.close();
+
+            //Tell the media scanner about the new file so that it is immediately available to the user.
+            MediaScannerConnection.scanFile(this,
+                    new String[]{file.toString()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                            Log.i("ExternalStorage", "Scanned " + path + ":");
+                            Log.i("ExternalStorage", "-> uri=" + uri);
+                        }
+                    });
+
+            //Set notification after download complete and add "click to view" action to that
+            String mimetype = url.substring(url.indexOf(":") + 1, url.indexOf("/"));
+            Intent intent = new Intent();
+            intent.setAction(android.content.Intent.ACTION_VIEW);
+            //............
+            Uri imgUri = FileProvider.getUriForFile(
+                    getApplicationContext(),
+                    getApplicationContext()
+                            .getPackageName() + ".provider", file);
+            intent.setDataAndType(imgUri, (mimetype + "/*"));
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            //..........
+            //intent.setDataAndType(Uri.fromFile(file), (mimetype + "/*"));
+            PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentText(getString(R.string.msg_file_downloaded))
+                    .setContentTitle(filename)
+                    .setContentIntent(pIntent)
+                    .build();
+
+            notification.flags |= Notification.FLAG_AUTO_CANCEL;
+            int notificationId = 85851;
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(notificationId, notification);
+        } catch (IOException e) {
+            Log.w("ExternalStorage", "Error writing " + file, e);
+            Toast.makeText(getApplicationContext(), R.string.error_downloading, Toast.LENGTH_LONG).show();
+        }
+
+        return file.toString();
     }
 }
